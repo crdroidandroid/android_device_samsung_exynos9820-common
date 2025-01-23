3a0b6ad6753e1bf44f879c7b9562ab9712ec66de
pedro <pxd.bin@gmail.com>
2025-01-23 17:26:21 -0300

    exynos9820-common: Explicitly set as an non-AB target
    We will be changing AB to be the default, existing non-AB targets
    need to explicitly set AB_OTA_UPDATER to false
    
    Test: th
    Change-Id: I86a76296937a6abab1b4495cd66a35144348aafd

--- common.mk
+++ common.mk
@@ -338,6 +338,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samsung
 
+# Update
+AB_OTA_UPDATER := false
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.samsung \
